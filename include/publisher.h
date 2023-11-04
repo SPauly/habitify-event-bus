84bb017c9b8420440daac2dc3ec0912060a0ccf5
SPauly <spau@hotmail.de>
2023-11-04 23:50:33 +0100

    Add Publisher to file

--- include/publisher.h
+++ include/publisher.h
@@ -85,6 +85,82 @@ class PublisherBase : public std::enable_shared_from_this<PublisherBase> {
   ChannelIdType channel_id_ = 0;
 };
 }  // namespace internal
+
+/// Publisher manages objects of type Event<EvTyp> and publishes the data to the
+/// Listener.
+/// It is designed to be thread safe so that multiple Listeners can access the
+/// data concurrently.
+/// Usage:
+///       std::unique_ptr<Event<int>> event;
+///       std::shared_ptr<Publisher<int>> p = Publisher<int>::Create();
+///       p->CreatePublisher(0);
+///       p.Publish(std::move(event));
+template <typename EvTyp>
+class Publisher : public internal::PublisherBase {
+ public:
+  friend class EventBus;
+
+  ~Publisher() = default;
+
+  // Publisher is not copyable due to the use of std::shared_mutex
+  Publisher(const Publisher&) = delete;
+  const Publisher& operator=(const Publisher&) = delete;
+
+  /// Publisher::HasReceivedEvent(size_t index) checks if there are unread
+  /// events for the Listener
+  virtual bool HasReceivedEvent(size_t index) override {
+    std::shared_lock<std::shared_mutex> lock(mux_);
+    return index < writer_index_;
+  }
+
+  /// Publisher<EvTyp>::Publish(std::unique_ptr< const internal::EventBase>)
+  /// takes ownership of the event and provides thread safe access to the
+  /// Listener.
+  template <typename T>
+  bool Publish(std::unique_ptr<const Event<T>> event) {
+    if (!get_is_registered()) return false;
+    std::unique_lock<std::shared_mutex> lock(mux_);
+
+    auto shared_event =
+        std::shared_ptr<const internal::EventBase>(std::move(event));
+    event_storage_.emplace(writer_index_, shared_event);
+
+    cv_->notify_all();
+    ++writer_index_;
+    return true;
+  }
+
+  inline const size_t get_writer_index() { return writer_index_; }
+
+ protected:
+  /// See PublisherBase::ReadLatestImpl()
+  virtual const std::shared_ptr<const internal::EventBase> ReadLatestImpl()
+      override {
+    std::shared_lock<std::shared_mutex> lock(mux_);
+
+    if (event_storage_.empty()) return nullptr;
+
+    auto event = event_storage_.find(writer_index_ - 1);
+    if (event == event_storage_.end()) return nullptr;
+
+    return event->second;
+  }
+
+ private:
+  Publisher() : PublisherBase() {}
+  /// Publisher()::Create() was made private to ensure that it is only created
+  /// via the EventBus::CreatePublisher() function. This way we can enforce
+  /// that Publisher is purely used as shared_ptr instance.
+  static std::shared_ptr<Publisher<EvTyp>> Create() {
+    return std::shared_ptr<Publisher<EvTyp>>(new Publisher<EvTyp>());
+  }
+
+ private:
+  std::unordered_map<int, std::shared_ptr<const internal::EventBase>>
+      event_storage_;
+  size_t writer_index_ = 0;
+};
+
 }  // namespace habitify
 
 #endif  // HABITIFY_EVENT_BUS_INCLUDE_PUBLISHER_H_
